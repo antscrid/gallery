ab734ee7937ee4a4dad793b14ddb4a08a8bfb4d1
= <=>
2018-04-23 14:40:23 +0300

    Updated index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
         <div class="gallery">
             <a data-fancybox="gallery" href= '<?php echo "https://fakeimg.pl/250x150/"?>'>
                 <img src="<?php echo "https://fakeimg.pl/250x150/"?>"></a>
