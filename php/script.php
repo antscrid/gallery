886553cb7b24f48d6e6d3cc7237270cd1177659e
= <=>
2018-05-04 13:56:07 +0300

    Created the massive with images data

--- php/script.php
+++ php/script.php
@@ -1,6 +1,72 @@
 <?php
-define ("Title", "Галерея", false);
 // Значення <title> додане в якості константи
-$Img_link='https://fakeimg.pl/250x150/';
+define ("Title", "Галерея", false);
 // Посилання на зображення додане, як змінна
+$Img1_link='https://picsum.photos/250/150?image=10';
+$Img2_link='https://picsum.photos/250/150?image=20';
+$Img3_link='https://picsum.photos/250/150?image=30';
+$Img4_link='https://picsum.photos/250/150?image=40';
+$Img5_link='https://picsum.photos/250/150?image=50';
+$Img6_link='https://picsum.photos/250/150?image=60';
+$Img7_link='https://picsum.photos/250/150?image=70';
+$Img8_link='https://picsum.photos/250/150?image=80';
+$Img9_link='https://picsum.photos/250/150?image=90';
+
+//Додано двохмірний масив з інформацією про зображення
+$images = array(
+    array(
+        'name' => 'forest',
+        'url' => 'https://picsum.photos/250/150?image=10',
+        'width' => '250',
+        'heigth' => '150',
+    ),
+    array(
+        'name' => 'table',
+        'url' => 'https://picsum.photos/250/150?image=20',
+        'width' => '250',
+        'heigth' => '150',
+    ),
+    array(
+        'name' => 'cup',
+        'url' => 'https://picsum.photos/250/150?image=30',
+        'width' => '250',
+        'heigth' => '150',
+    ),
+    array(
+        'name' => 'cat',
+        'url' => 'https://picsum.photos/250/150?image=40',
+        'width' => '250',
+        'heigth' => '150',
+    ),
+    array(
+        'name' => 'bird',
+        'url' => 'https://picsum.photos/250/150?image=50',
+        'width' => '250',
+        'heigth' => '150',
+    ),
+    array(
+        'name' => 'computer',
+        'url' => 'https://picsum.photos/250/150?image=60',
+        'width' => '250',
+        'heigth' => '150',
+    ),
+    array(
+        'name' => 'trees',
+        'url' => 'https://picsum.photos/250/150?image=70',
+        'width' => '250',
+        'heigth' => '150',
+    ),
+    array(
+        'name' => 'pinecone',
+        'url' => 'https://picsum.photos/250/150?image=80',
+        'width' => '250',
+        'heigth' => '150',
+    ),
+    array(
+        'name' => 'jars',
+        'url' => 'https://picsum.photos/250/150?image=90',
+        'width' => '250',
+        'heigth' => '150',
+    ),
+);
 
