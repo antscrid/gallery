757f4ec9a728ff044d97a2371210bf7f19699c4b
= <=>
2018-05-11 00:41:58 +0300

    Created the functions for operating the image data

--- index.php
+++ index.php
@@ -3,30 +3,40 @@
 	<head>
         <link rel="shortcut icon" href="http://www.iconarchive.com/download/i97958/thehoth/seo/seo-web-code.ico" type="image/ico">
         <link rel="stylesheet" href="../css/css.css">
+        <link rel="stylesheet" href="../css/bootstrap.css">
+        <link rel="stylesheet" href="../css/main.css">
         <?php
-        include_once 'php/script.php';
-        include_once 'php/json_array.php';
+        //include_once 'php/script.php';
+        include 'php/json_array.php';
         ?>
         <title><?php
-            if ( (isset($Img_link)) && (defined('Title')) ) {
-                echo 'Фото '.Title;
-            }
             // Перевірка оголошення константи і змінної, виведення константи в  <title>
-            ?>
+            if ((isset($data['url'])) && (defined('Title')) ) {
+                echo 'Фото '. Title;
+            }?>
         </title>
     </head>
 	<body>
     <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
-    <div class="right"></div>
     <div class="gallery">
-        <!-- Цикл для перебору елементів отриманого масиву з 9 елементів та їх виведення у посиланнях і картинках -->
-        <?php foreach($json_part as $key => $value){
-               echo "<a data-fancybox=\"gallery\" href=". $value['post_url'] . '/download' .">" . "<img src=". $value['post_url'] . '/download' .">" . "</a>";
-    };
-    ?>
+        <!--Перебираємо масив для виведення елементів, які сформували на сторінці json_array-->
+        <?php foreach ($pics as $data){ ?>
+            <div>
+                <div class="box">
+                    <a data-fancybox="gallery"
+                       href="<?php echo $data['url'] ?>">
+                        <img class src="<?php echo $data['thumbnail'] ?>">
+                    </a>
+                    <div class="text-box">
+                        <p><b>Created by: </b><?php echo $data['author'] ?></p>
+                        <p><b>Created at: </b><?php echo $data['pics_date'] ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php }; ?>
+
     </div>
-    <div class="right"></div>
     </body
 </html>
--- php/json_array.php
+++ php/json_array.php
@@ -1,19 +1,120 @@
 <?php
+define ("Title", "Галерея", false);
 //Імпорт даних з json-файлу
-$json_file = file_get_contents ('https://picsum.photos/list');
+    $json_file = file_get_contents('https://picsum.photos/list');
 
 //Розшифрування json в якості асоціативного масиву
-$jsonarray = json_decode($json_file,TRUE);
+    $jsonarray = json_decode($json_file, TRUE);
 
 //Вибір 9 елементів масиву, починаючи з 257-го елемента
-$json_part = array_slice($jsonarray, 256, 9, true);
+    $json_part = array_slice($jsonarray, 257, 9, true);
 
-//Створення функції для сортування елементів масиву по значенню 'filename'
-function sortfunnction($a, $b){
+    //Створення нового масиву для зручного оперування і додавання ключів та значень
+    $pics = [];
+function date_and_time()
+{
+    return date('d M Y H:i:s', time());
+}
+//Створення функції для сортування елементів масиву по даті й часу
+function sortfunction($a, $b)
+{
     if ($a['filename'] == $b['filename']) {
         return 0;
     }
     return ($a['filename'] < $b['filename']) ? -1 : 1;
 }
-usort($json_part, "sortfunnction");
-
+usort($json_part, "sortfunction");
+    //Перебираємо масив для виведення елементів
+    foreach ($json_part as $key => $value) {
+        $img_url = ($value['post_url'] . '/download');
+        $size = getimagesize("$img_url");
+        $pics[] = [
+            "url" => $img_url,
+            //"thumbnail" => generateThumbnail($img_url, $width, $height),
+            "thumbnail" => $img_url,
+            "author" => $value['author'],
+            "width" => $value['width'],
+            "height" => $value['height'],
+            //Виведення масиву про дату через ф-цю
+            "pics_date" => date_and_time(),
+            //Виведення масиву про дату через ф-цю
+            //'size' => $size,
+        ];
+        //Умова для перевірки наявності шляху до зображення
+    }
+function imageExists($imagePath)
+{
+    if (isset($imagePath) && !empty($imagePath)) {
+        return $imagePath;
+    } else {
+        return "https://picsum.photos/250/150?image=20";
+    }
+}
+/** Generate image thumbnail in base64
+ *
+ * @param $imagePath
+ * @param $width
+ * @param $height
+ * @return string
+ */
+function generateThumbnail($imagePath, &$width, &$height)
+{
+    $params = getOriginalSize($imagePath);
+    return 'data:' . $params['mime'] . ';base64,' . base64_encode(resizeImage($imagePath, $width, $height, $params));
+}
+/** Resize Image
+ *
+ * @param $imagePath
+ * @param $width
+ * @param $height
+ * @param $params
+ * @return string
+ */
+function resizeImage($imagePath, &$width, &$height, $params)
+{
+    $mime = $params['mime'];
+    //use specific function based on image format
+    switch ($mime) {
+        case 'image/jpeg':
+            $imageCreateFunc = 'imagecreatefromjpeg';
+            $imageSaveFunc = 'imagejpeg';
+            break;
+        case 'image/png':
+            $imageCreateFunc = 'imagecreatefrompng';
+            $imageSaveFunc = 'imagepng';
+            break;
+        case 'image/gif':
+            $imageCreateFunc = 'imagecreatefromgif';
+            $imageSaveFunc = 'imagegif';
+            break;
+        default:
+            //we will handle this once work with errors
+    }
+    //Variable function
+    $img = $imageCreateFunc($imagePath);
+    //list is php construction that allows to set array elements to variables
+    list($originalWidth, $originalHeight) = $params;
+    //calculate height
+    if (!$height) {
+        $height = ($originalHeight / $originalWidth) * $width;
+    }
+    //create new image
+    $bufferImage = imagecreatetruecolor($width, $height);
+    imagecopyresampled($bufferImage, $img, 0, 0, 0, 0, $width, $height, $originalWidth, $originalHeight);
+    //save original image size to variables for later use outside of function
+    $width = $originalWidth;
+    $height = $originalHeight;
+    //return buffer output as string
+    ob_start();
+    $imageSaveFunc($bufferImage);
+    return ob_get_clean();
+}
+/** get image size info
+ *
+ * @param $imagePath
+ * @return array|bool
+ */
+function getOriginalSize($imagePath)
+{
+    return getimagesize($imagePath);
+}
